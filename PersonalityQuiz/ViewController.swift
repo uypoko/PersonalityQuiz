4338a9a510c7d7cd60e7c4b6421e95a03f17a3de
Ryan <cungdinhuy@gmail.com>
2019-05-29 16:04:09 +0700

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Uy Cung Dinh on 5/29/19.
+//  Copyright © 2019 Equity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
